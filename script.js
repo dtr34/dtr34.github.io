3bc5e0d0ceb5f5ff9b245fa6cf46180529751023
daniel <dtr@vt.edu>
2024-08-17 17:14:15 -0400

    first attempt to set up landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+function startGame() {
+    alert("Starting the game...");
+    // You can replace this with actual functionality to load the game.
+    // e.g., window.location.href = "game.html"; 
+}
+
